2496ce79566213aed2541559761f5d9d7c90d3f5
PAHANICH <pashanich2016@mail.ru>
2016-05-19 00:26:42 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Zayc
+Mutable
---
+++ src/test/java/de/davherrmann/immutable/ImmutableTypeAdapterTest.java
@@ -0,0 +1,29 @@
+package de.davherrmann.immutable;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+public class ImmutableTypeAdapterTest
+{
+    private final Immutable<POJO> immutable = new Immutable<>(POJO.class);
+    private final POJO path = immutable.path();
+
+    @Test
+    public void wrappingImmutableTypeAdapterFactory_works() throws Exception
+    {
+        // given / when
+        final Immutable<POJO> newImmutable = immutable.in(path::pojo).set(immutable.asObject());
+
+        // then
+        assertThat(new Gson().toJson(newImmutable), is("{\"pojo\":{}}"));
+    }
+
+    private interface POJO
+    {
+        POJO pojo();
+    }
+}
